65e4c048b0e1a60f9680084932cac19f0770e8f8
kulk <roelandkulk@gmail.com>
2019-03-21 22:04:16 +0100

    user login and registration added

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired()])
@@ -11,3 +12,14 @@ class LoginForm(FlaskForm):
 class TodoForm(FlaskForm):
 	task = StringField('New task', validators=[DataRequired()])
 	submit = SubmitField('Add task')
+
+class RegistrationForm(FlaskForm):
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	password = PasswordField('Password', validators=[DataRequired()])
+	password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+	submit = SubmitField('Register')
+
+	def validate_email(self, email):
+		user = User.query.filter_by(email=email.data).first()
+		if user is not None:
+			raise ValidationError('Please use a different email address.')
--- app/models.py
+++ app/models.py
@@ -1,7 +1,9 @@
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	email = db.Column(db.String(120), index=True, unique=True)
 	password_hash = db.Column(db.String(128))
@@ -10,8 +12,18 @@ class User(db.Model):
 	def __repr__(self):
 		return '<User %s>' % self.email
 
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
 class Task(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	body = db.Column(db.String(160))
 	timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 	user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # user is the name of the table of the user model
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
